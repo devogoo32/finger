18537bd344e766e80860fd3068b76963b3aadc3a
Denilson <dlemus@harding.edu>
2023-02-14 20:54:13 -0600

    🚧 Wrok on navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <img className="logo" src="#" alt="logo" />
+      </div>
+      <button className="navbar__button">Log in</button>
+    </nav>
+  );
+}
+
+export default Navbar;
