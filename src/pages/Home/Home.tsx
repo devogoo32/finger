4c3a97dd2e4be257a3cf3413f3daec12a506b513
Denilson <dlemus@harding.edu>
2023-02-15 16:21:43 -0600

    💄 Style home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import "./Home.css";
 import Modal from "../../components/Modal/Modal";
+import "./Home.css";
 
 function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleClick = () => {
-    console.log("CLICK");
     setIsModalOpen(true);
   };
 
@@ -19,8 +18,8 @@ function Home() {
         <button className="finger-button" onClick={handleClick}>
           Let's Play
         </button>
+        {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
       </div>
-      {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
     </>
   );
 }
