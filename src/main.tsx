859dc65fd854ceccbe52a8c3215f7449228ff92d
Denilson <dlemus@harding.edu>
2023-02-16 18:47:39 -0600

    ✨ Add dashboard route

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
   },
 ]);
 
