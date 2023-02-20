9337f6f78ddff8ba99f804f38281ad279864dce8
Denilson <dlemus@harding.edu>
2023-02-20 16:11:06 -0600

    ✨ Add control buttons

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,9 +1,19 @@
+import ThumbUpFillIcon from 'remixicon-react/ThumbUpFillIcon';
+import ThumbDownFillIcon from 'remixicon-react/ThumbDownFillIcon';
 import "./Controls.css";
 
 function Controls() {
   return (
-    <div className="controls">Controls</div>
-  )
+    <div className="controls">
+      <div className="controls__button">
+        <ThumbDownFillIcon size={35} />
+      </div>
+
+      <div className="controls__button">
+        <ThumbUpFillIcon size={35} />
+      </div>
+    </div>
+  );
 }
 
-export default Controls
+export default Controls;
