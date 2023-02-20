796fa8c59b483ef9525a8f974eab7e8ed35cd497
Denilson <dlemus@harding.edu>
2023-02-20 14:34:15 -0600

    💄 Style sidebar header

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,17 @@
 import "./Sidebar.css";
 
 function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <h1>Finger</h1>
+      </div>
+      <div className="sidebar__subheader">
+        <h3>Matches</h3>
+        <div className="sidebar__counter">3</div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
