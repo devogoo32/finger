9b34af1123eefb1b9377a8d97d5b1e155fdc8a2a
Denilson <dlemus@harding.edu>
2023-02-21 16:24:54 -0600

    ♻️ Refactor sidebar toggle

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import Match from "../Match/Match";
-import ArrowRightSLineIcon from "remixicon-react/ArrowRightSLineIcon";
+import MenuFillIcon from "remixicon-react/MenuFillIcon";
 import "./Sidebar.css";
 
 const characters = [
@@ -15,23 +15,27 @@ const characters = [
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <div className="sidebar__header">
-        <h1>Finger</h1>
-        <div className="sidebar__toggle">
-          <ArrowRightSLineIcon size={25} />
+    <>
+      <div className="toggle">
+        <div>
+          <MenuFillIcon size={30} />
         </div>
       </div>
-      <div className="sidebar__subheader">
-        <h3>Matches</h3>
-        <div className="sidebar__counter">3</div>
+      <div className="sidebar">
+        <div className="sidebar__header">
+          <h1>Finger</h1>
+        </div>
+        <div className="sidebar__subheader">
+          <h3>Matches</h3>
+          <div className="sidebar__counter">3</div>
+        </div>
+        <div className="sidebar__matches">
+          {characters.map((match) => (
+            <Match image={match.url} name={match.name} />
+          ))}
+        </div>
       </div>
-      <div className="sidebar__matches">
-        {characters.map((match) => (
-          <Match image={match.url} name={match.name} />
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
 
