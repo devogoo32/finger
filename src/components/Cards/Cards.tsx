0b160c7c6ea4ece35667147ea77bc3b251438982
Denilson <dlemus@harding.edu>
2023-02-21 12:51:23 -0600

    💄 Style card bio

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -5,10 +5,12 @@ const characters = [
   {
     name: "Richard Hendricks",
     url: "https://m.media-amazon.com/images/M/MV5BZTE5MzdlNzItNWQ2MC00NjU5LWJiYjQtYjNjYzMyN2ZlNTkyXkEyXkFqcGdeQXVyNjc5Mjg0NjU@._V1_UY1200_CR585,0,630,1200_AL_.jpg",
+    bio: "Lorem ipsumLorem ipsumLorem ipsum",
   },
   {
     name: "Monica Hall",
     url: "https://s1.r29static.com/bin/entry/006/x,80/1945796/image.jpg",
+    bio: "Lorem ipsum",
   },
 ];
 
@@ -21,7 +23,10 @@ function Cards() {
             style={{ backgroundImage: "url(" + character.url + ")" }}
             className="card"
           >
-            <h3>{character.name}</h3>
+            <div className="card__info">
+              <h2>{character.name}</h2>
+              <p>{character.bio}</p>
+            </div>
           </div>
         </TinderCard>
       ))}
