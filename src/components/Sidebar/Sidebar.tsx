5087dc450df3369fbcf39245232f8ad0e67f0047
Denilson <dlemus@harding.edu>
2023-02-25 23:32:00 -0600

    ♻️ Refactor sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,27 +5,24 @@ import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <>
-      <div className="toggle">
-        <div>
-          <MenuFillIcon size={30} />
-        </div>
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <h1>Finger</h1>
       </div>
-      <div className="sidebar">
-        <div className="sidebar__header">
-          <h1>Finger</h1>
-        </div>
-        <div className="sidebar__subheader">
-          <h3>Matches</h3>
-          <div className="sidebar__counter">3</div>
-        </div>
-        <div className="sidebar__matches">
-          {characters.map((character) => (
-            <Match key={character.id} image={character.url} name={character.name} />
-          ))}
-        </div>
+      <div className="sidebar__subheader">
+        <h3>Matches</h3>
+        <div className="sidebar__counter">{characters.length}</div>
       </div>
-    </>
+      <div className="sidebar__matches">
+        {characters.map((character) => (
+          <Match
+            key={character.id}
+            image={character.url}
+            name={character.name}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
