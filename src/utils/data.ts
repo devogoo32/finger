43528c469f336a0e9882153c5610fa03f8ed1772
Denilson <dlemus@harding.edu>
2023-02-28 14:11:49 -0600

    🗃️ Add men characters info

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,12 +1,39 @@
-export const characters = [
+export const menCharacters = [
   {
     id: 0,
-    name: "Richard Hendricks",
-    url: "https://m.media-amazon.com/images/M/MV5BZTE5MzdlNzItNWQ2MC00NjU5LWJiYjQtYjNjYzMyN2ZlNTkyXkEyXkFqcGdeQXVyNjc5Mjg0NjU@._V1_UY1200_CR585,0,630,1200_AL_.jpg",
-    bio: "Lorem ipsumLorem ipsumLorem ipsum",
+    name: "Groot",
+    url: "src/assets/groot.png",
+    bio: "I am Groot",
   },
   {
     id: 1,
+    name: "Yoda",
+    url: "src/assets/yoda.png",
+    bio: "Size matters not, but swipe right, you must",
+  },
+  {
+    id: 2,
+    name: "Forrest Gump",
+    url: "src/assets/forrest.png",
+    bio: "I'm a former college football player, shrimp boat captain, and Vietnam veteran. Seeking someone who can appreciate my love of running and Ping-Pong. I may not be a smart man, but I know what love is.",
+  },
+  {
+    id: 3,
+    name: "Mclovin",
+    url: "src/assets/mclovin.png",
+    bio: "Hawain. Organ donor",
+  },
+  {
+    id: 4,
+    name: "Borat Sagdiyev",
+    url: "src/assets/borat.png",
+    bio: "Kazakh journalist and world traveler seeking partner for cultural exchange and sexy time.",
+  },
+];
+
+export const womenCharacter = [
+  {
+    id: 0,
     name: "Monica Hall",
     url: "https://s1.r29static.com/bin/entry/006/x,80/1945796/image.jpg",
     bio: "Lorem ipsum",
